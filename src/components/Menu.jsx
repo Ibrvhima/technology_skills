86555fb97f4b3441e186825735a9749b1584a7e3
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-04-23 12:23:51 +0000

    new commit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,49 +2,63 @@ import { NavLink } from "react-router-dom";
 
 function Menu() {
   return (
-    <div className="container p-3 rounded mt-3 bg-primary shadow">
-      <ul className="d-flex justify-content-center fs-5 list-unstyled m-0">
-        <li className="mx-4">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `nav-link px-3 py-2 rounded fw-semibold ${
-                isActive ? "bg-light text-primary" : "text-white"
-              }`
-            }
-            style={{ textDecoration: "none" }}
-          >
-            Accueil
-          </NavLink>
-        </li>
-        <li className="mx-4">
-          <NavLink
-            to="form"
-            className={({ isActive }) =>
-              `nav-link px-3 py-2 rounded fw-semibold ${
-                isActive ? "bg-light text-primary" : "text-white"
-              }`
-            }
-            style={{ textDecoration: "none" }}
-          >
-            Ajouter Techno
-          </NavLink>
-        </li>
-        <li className="mx-4">
-          <NavLink
-            to="list"
-            className={({ isActive }) =>
-              `nav-link px-3 py-2 rounded fw-semibold ${
-                isActive ? "bg-light text-primary" : "text-white"
-              }`
-            }
-            style={{ textDecoration: "none" }}
-          >
-            Liste des Technos
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-primary px-3 mt-3 rounded shadow">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand text-white fw-bold" to="/">
+          TechnoSkills
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item mx-2">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  `nav-link fw-semibold rounded px-3 py-2 ${
+                    isActive ? "bg-light text-primary" : "text-white"
+                  }`
+                }
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li className="nav-item mx-2">
+              <NavLink
+                to="/form"
+                className={({ isActive }) =>
+                  `nav-link fw-semibold rounded px-3 py-2 ${
+                    isActive ? "bg-light text-primary" : "text-white"
+                  }`
+                }
+              >
+                Ajouter Techno
+              </NavLink>
+            </li>
+            <li className="nav-item mx-2">
+              <NavLink
+                to="/list"
+                className={({ isActive }) =>
+                  `nav-link fw-semibold rounded px-3 py-2 ${
+                    isActive ? "bg-light text-primary" : "text-white"
+                  }`
+                }
+              >
+                Liste des Technos
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
